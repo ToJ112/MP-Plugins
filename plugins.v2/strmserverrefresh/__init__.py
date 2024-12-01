ec50b1e23792fb67a96ec71834a2d2ee1110f07c
TOJ112 <jtning@bupt.edu.cn>
2024-12-01 18:24:53 +0800

    init

--- plugins.v2/strmserverrefresh/__init__.py
+++ plugins.v2/strmserverrefresh/__init__.py
@@ -20,7 +20,7 @@ class StrmServerRefresh(_PluginBase):
     # 插件图标
     plugin_icon = "refresh.png"
     # 插件版本
-    plugin_version = "0.0.1"
+    plugin_version = "0.0.2"
     # 插件作者
     plugin_author = "jtning"
     # 作者主页
