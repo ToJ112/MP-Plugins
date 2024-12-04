2e414e8412b3f6e0242e66c8dabb1e51d75c3493
TOJ112 <jtning@bupt.edu.cn>
2024-12-05 00:21:02 +0800

    init

--- plugins.v2/strmserverrefresh/__init__.py
+++ plugins.v2/strmserverrefresh/__init__.py
@@ -29,7 +29,7 @@ class StrmServerRefresh(_PluginBase):
     # 插件配置项ID前缀
     plugin_config_prefix = "strmserverrefresh_"
     # 加载顺序
-    plugin_order = 99
+    plugin_order = 9
     # 可使用的用户级别
     auth_level = 1
 
